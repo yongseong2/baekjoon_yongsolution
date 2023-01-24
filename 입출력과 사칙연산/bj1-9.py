c1ca9e922f74453a6a769249722da2124f6077f3
{yongseong2} <{ancjs369@naver.com}>
2023-01-24 21:22:30 +0900

    정리

--- 입출력과 사칙연산/bj1-9.py
+++
@@ -1,5 +0,0 @@
-#킹,퀸,룩,비숍,나이트,폰 틀림
-chess = [1,1,2,2,2,8]
-a=list(map(int,input().split()))
-for i in range(6):
-    print(chess[i]-a[i], end=" ")
--- 조건문/bj2-1.py
+++
@@ -1,8 +0,0 @@
-#두 수 비교하기
-A,B=map(int,input().split())
-if A>B:
-    print('>')
-elif A<B:
-    print('<')
-elif A==B:
-    print('==')

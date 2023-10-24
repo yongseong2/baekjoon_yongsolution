6ff6e0fcf26f5f0c8d6f11408e7ca11bb93ef3eb
yongseong2 <122865525+yongseong2@users.noreply.github.com>
2023-10-24 23:10:38 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,14 @@
+def solution(brown, yellow):
+    answer = []
+    yello_x = 0
+    yello_y = 0
+    for i in range(1, yellow+1):
+        if yellow % i == 0:
+            yellow_x = int(yellow/i)
+            yellow_y = i
+            if yellow_x*2 + yellow_y*2 + 4 == brown:
+                answer.append(yellow_x + 2)
+                answer.append(yellow_y + 2)
+                return sorted(answer, reverse=True)
+    
+    return answer
